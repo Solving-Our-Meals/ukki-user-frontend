54675be3ff2488903d95f37390b385a3131df295
JUHYE0925 <rhkdlf48301@gmail.com>
2024-12-16 17:08:02 +0900

    feature:floatingbar/2024-12-16

--- src/user/layouts/UserLayout.js
+++ src/user/layouts/UserLayout.js
@@ -2,6 +2,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../common/header/components/Header";
 import Footer from "../../common/footer/components/Footer";
+import FloatingBar from "../../common/floatingbar/components/FloatingBar";
 
 function UserLayout(){
 
@@ -9,6 +10,7 @@ function UserLayout(){
         <>
             <Header/>
                 <main>
+                    <FloatingBar/>
                     <Outlet />
                 </main>
             <Footer/>   
