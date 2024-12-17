a03aed0a7b518b53f7e68e26879e2a7f85a41b46
Pear1yCode <gh_wns@naver.com>
2024-12-17 16:19:39 +0900

    feat: Signup/2024-12-17

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Signup from './user/pages/Signup';
-import Layout from './common/header/layouts/Layouts';
-import DoInquiry from './common/inquiry/components/doinquiry'
+import Signup from '../src/user/pages/signup/pages/Signup';
+import Info from './user/pages/info/Info';
+import UserLayout from './user/layouts/UserLayout';
+import StoreDetail from './user/pages/storedetail/pages/StoreDetail';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="auth/login" element={<Signup/>}/>
+      <Route path="auth/signup" element={<Signup/>}/>
       <Route></Route>
-      <Route path="/" element={<Layout/>}>
+      <Route path="/" element={<UserLayout/>}>
+      <Route path="info" element={<Info/>}/>
+      <Route path="store" element={<StoreDetail/>}/>
       </Route>
     </Routes>
     </BrowserRouter>
